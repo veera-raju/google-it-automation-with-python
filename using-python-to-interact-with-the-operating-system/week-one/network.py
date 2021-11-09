4fb001956e7461800ed253a48d44e283e4c34c57
veera <77234681+veera-raju@users.noreply.github.com>
2021-11-09 22:37:37 +0800

    Update network.py

--- using-python-to-interact-with-the-operating-system/week-one/network.py
+++ using-python-to-interact-with-the-operating-system/week-one/network.py
@@ -2,10 +2,11 @@
 import requests
 import socket
 def check_localhost():
-    localhost = socket.gethostbyname('localhost')
-    if localhost == '127.0.0.1':
-        return True
-    return False
+    def gethostbyname():
+        localhost = socket.gethostbyname('localhost')
+        if localhost == '127.0.0.1':
+            return True
+        return False
 def check_connectivity():
     request = requests.get('http://www.google.com')
     response = request.status_code
